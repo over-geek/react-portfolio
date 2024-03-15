13950414c0a8cf8f55b6db8ea2a03778cd301882
over-geek <kenagy100@gmail.com>
2024-03-15 13:32:54 +0000

    Add layout and sidebar components

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import '../styles/Dynamic_text.scss'
+import Home from './Home';
+
+const Layout = () => {
+	return (
+		<div className='layout'>
+			<Home />
+		</div>
+	)
+}
+
+export default Layout;
