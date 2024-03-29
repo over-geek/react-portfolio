618ddedd19a90b8d5e06fe0d5ccc446027db3086
over-geek <kenagy100@gmail.com>
2024-03-29 04:01:33 +0000

    Add lightMode feature

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Layout from '../components/Layout';
 import '../styles/Mainpage.css';
 import '../styles/Mobile.css';
 
 function MainPage() {
+	const [lightMode, setLightMode] = useState(() => {
+		const savedLightMode = localStorage.getItem('lightMode');
+		return savedLightMode ? JSON.parse(savedLightMode) : false;
+	});
+
+	const toggleLightMode = () => {
+		const newLightMode = (!lightMode);
+		setLightMode(newLightMode);
+		localStorage.setItem('lightMode', JSON.stringify(newLightMode));
+	}
+
 	return (
-		<div className='main'>
-			<Navbar />
+		<div className={`main ${lightMode ? 'light-mode' : ''}`}>
+			<Navbar toggleLightMode={toggleLightMode} />
 			<Layout />
 		</div>
 	)
